35c4fce97d4b98e000142c3154e2ec44bdec3d7f
xlebnik <glebrumancev430@gmail.com>
2026-09-12 12:28:21 +0300

    Ввод числа.

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main()
+        {
+            long n = Vvod();
+            Obratnoe(n);
+        }
+        static long Vvod()
+        {
+            long n;
+
+            do
+            {
+                Console.WriteLine("Введите натуральное число:");
+            }
+            while (!long.TryParse(Console.ReadLine(), out n)  n <= 0);
+
+            return n;
+        }
+
+    }
+}
