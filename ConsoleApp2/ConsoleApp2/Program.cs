b1f871ab6e4b4591ba61078f5773eb1a2bd69b05
xlebnik <glebrumancev430@gmail.com>
2026-09-12 11:42:37 +0300

    Ввод и проверка натурального числа.

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace ConsoleApp2
 {
     internal class Program
     {
+       /// 1 Задание.
         static long ReadNaturalNumber()
         {
             while (true)
@@ -44,4 +46,26 @@ namespace ConsoleApp2
         }
     }
 }
-
+/// 2 Задание.
+class Program
+{
+    static void Main()
+    {
+        long number = ReadNumber();
+        long reversed = ReverseNumber(number);
+        Printresult(reversed);
+    }
+    static long ReadNumber()
+    {
+        long number;
+        while (true)
+        {
+            Console.Write("Введите натуральное число:");
+            if (long.TryParse(Console.ReadLine(), out number) && number > 0)
+            {
+                return number;
+            }
+            Console.WriteLine("Ошибка, введите натуральное число больше нуля!")
+        }
+    }
+}
