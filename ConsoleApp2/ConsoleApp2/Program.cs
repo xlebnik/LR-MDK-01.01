9ad4e4aa52bb5535ab1ce2392f143fe8cea83663
xlebnik <glebrumancev430@gmail.com>
2026-09-12 11:54:45 +0300

    .

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -46,26 +46,3 @@ namespace ConsoleApp2
         }
     }
 }
-/// 2 Задание.
-class Program
-{
-    static void Main()
-    {
-        long number = ReadNumber();
-        long reversed = ReverseNumber(number);
-        Printresult(reversed);
-    }
-    static long ReadNumber()
-    {
-        long number;
-        while (true)
-        {
-            Console.Write("Введите натуральное число:");
-            if (long.TryParse(Console.ReadLine(), out number) && number > 0)
-            {
-                return number;
-            }
-            Console.WriteLine("Ошибка, введите натуральное число больше нуля!")
-        }
-    }
-}
